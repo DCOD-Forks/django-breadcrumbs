a95f8bb2ccf89f62e7c7bb32b99f892706e4e841
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2016-11-25 13:23:12 +0200

    Update middlewares for django 1.10

--- breadcrumbs/middleware.py
+++ breadcrumbs/middleware.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 from django.http import Http404
+from django.utils.deprecation import MiddlewareMixin
 from .breadcrumbs import Breadcrumbs
 from .views import flatpage
 
 
-class BreadcrumbsMiddleware(object):
+class BreadcrumbsMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
         request.breadcrumbs = Breadcrumbs()
         request.breadcrumbs._clean()
 
 
-class FlatpageFallbackMiddleware(object):
+class FlatpageFallbackMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         # do nothing if flatpages middleware isn't enabled, also if response
         # code isn't 404.
